b2b705d904a1618ff835542c1e57304d28a24ae1
Bryan Robinson <bryanlrobinson@gmail.com>
2021-09-13 11:19:46 -0400

    Gets files

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+
+    // Get diff files
+    const files = github.context.payload.pull_request.files;
+    console.log(`The files: ${files}`);
+
+
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
+
+
+
 
 } catch(error) {
     core.setFailed(error.message);
